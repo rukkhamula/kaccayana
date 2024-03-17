bb85277d61701f80ebd395c21e3fcfa6321cb194
rukkhamula <163368356+rukkhamula@users.noreply.github.com>
2024-03-18 00:52:22 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,6 @@ language = 'en'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_book_theme"
+html_theme = "sphinx-better-theme"
 
 
